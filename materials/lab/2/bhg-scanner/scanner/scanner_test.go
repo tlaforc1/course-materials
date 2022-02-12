3fced0ae48ca6be0e6897469396907647c9bc588
Taylor LaForce <89807972+tlaforc1@users.noreply.github.com>
2022-02-11 21:32:29 -0700

    Update scanner_test.go

--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -6,25 +6,26 @@ import (
 
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
 func TestOpenPort(t *testing.T){
+	testVar := 23
+	want := 0
+    open, _ := PortScanner(testVar) // Currently function returns only number of open ports
+	if testVar >= 22{
+		want = 1
+	}	// default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
+	      //consider what would happen if you parameterize the portscanner address and ports to scan
 
-    got := PortScanner() // Currently function returns only number of open ports
-    want := 2 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
-	          //consider what would happen if you parameterize the portscanner address and ports to scan
-
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
+    if open != want {
+        t.Errorf("got %d, wanted %d", open, want)
     }
 }
 
 func TestTotalPortsScanned(t *testing.T){
 	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
+	testVar := 20
+    open, closed := PortScanner(testVar) // Currently function returns only number of open ports
+    want := testVar // default value; consider what would happen if you parameterize the portscanner ports to scan
 
-    got := PortScanner() // Currently function returns only number of open ports
-    want := 1024 // default value; consider what would happen if you parameterize the portscanner ports to scan
-
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
+    if open+closed != want {
+        t.Errorf("got %d, wanted %d", open+closed, want)
     }
 }
-
-
